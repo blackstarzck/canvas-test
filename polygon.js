5ec141c6297c02e7249e79b8f1fabf7bbba67664
chanki <bucheongosok@gmail.com>
2022-06-09 12:05:23 +0900

    완료

---
+++ polygon.js
@@ -0,0 +1,45 @@
+const PI2 = Math.PI * 2;
+
+export class Polygon {
+    constructor(x, y, radius, sides){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.sides = sides;
+        this.rotate = 0;
+
+        console.log(`FROM: x: ${x} | y: ${y}`);
+    }
+
+    animate(ctx, moveX){
+        ctx.save();
+        ctx.fillStyle = '#000';
+        // ctx.beginPath();
+
+        const angle = PI2 / this.sides;
+        console.log(angle);
+
+        ctx.translate(this.x, this.y);
+
+        this.rotate -= moveX * 0.005;
+
+        ctx.rotate(this.rotate);
+
+        for(let i = 0; i < this.sides; i++){
+            const x = this.radius * Math.cos(angle * i);
+            const y = this.radius * Math.sin(angle * i);
+
+            // (i == 0) ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
+
+            ctx.beginPath();
+            ctx.arc(x, y, 10, 0, PI2, false);
+            ctx.fill();
+
+            console.log(`[${i+1}] x: ${x} | y: ${y}`);
+        }
+
+        // ctx.fill();
+        // ctx.closePath();
+        ctx.restore();
+    }
+}
